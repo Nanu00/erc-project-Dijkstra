d41eacb0418fe5cde21f28280251bb576894969a
Shantanu Deshmukh <66585423+Nanu00@users.noreply.github.com>
2022-04-12 10:17:34 +0530

    Added hashability to Point class

--- utils.py
+++ utils.py
@@ -15,3 +15,14 @@ class Point(shapely.geometry.Point):
         new_x = random.random()*abs(boundaries[0].x - boundaries[1].x) + min(boundaries[0].x, boundaries[1].x)
         new_y = random.random()*abs(boundaries[0].y - boundaries[1].y) + min(boundaries[0].y, boundaries[1].y)
         return cls(new_x, new_y)
+
+    def __key(self):
+        return (self.x, self.y)
+
+    def __hash__(self):
+        return hash(self.__key())
+
+    def __eq__(self, other):
+        if isinstance(other, Point):
+            return self.__key() == other.__key()
+        return False
