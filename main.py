72ba1531291ac00637e212112a4ed5778a905a06
Shantanu Deshmukh <66585423+Nanu00@users.noreply.github.com>
2022-04-12 10:17:51 +0530

    Implemented PRM base

---
+++ main.py
@@ -0,0 +1,23 @@
+from prm import Graph
+from obstacle import EndArea, Obstacle
+from utils import Point as P
+from matplotlib import pyplot as plt
+
+# Define all the obstables
+obs = [ Obstacle((P(600, 700), P(700, 600), P(700, 700))) ]
+
+# Define the end area
+end = EndArea((P(700, 700), P(750, 700), P(750, 750), P(700, 750)))
+
+g = Graph(P(500, 500), obs, end, (P(0, 0), P(1000, 1000)))
+
+g.sample(100)
+
+plottables = g.get_plottable()
+plottables += [o.get_plottable() for o in g.obstacles]
+plottables.append(end.get_plottable())
+
+plt.ion()
+[plt.plot(*p, marker='.', markersize=1, linewidth=0.1) for p in plottables]
+plt.show()
+plt.pause(100)
--- prm.py
+++ prm.py
@@ -1,1 +1,33 @@
+import networkx as nx
+from utils import Point
+from obstacle import Obstacle, EndArea
 
+class Graph(nx.Graph):
+    def __init__(self, start_point: Point, obstacles: list[Obstacle], end_zone: EndArea, boundaries: tuple[Point, Point]):
+        super().__init__()
+        self.add_node(start_point)
+        self.obstacles = obstacles
+        self.end_zone = end_zone
+        self.boundaries = boundaries
+
+    def sample(self, num: int):
+        for _ in range(num):
+            new_point = Point.random_with_boundaries(self.boundaries)
+
+            outside = [o.pol.contains(new_point) for o in self.obstacles]
+            if any(outside):
+                break
+
+            self.add_node(new_point)
+
+            for node in self.nodes:
+                intersections = [o.checkint(node, new_point) for o in self.obstacles]
+                if any(intersections):
+                    continue
+                self.add_edge(node, new_point)
+
+    def get_plottable(self):
+        plottables = []
+        for (u, v) in self.edges:
+            plottables.append(([u.x, v.x], [u.y, v.y], "b-"))
+        return plottables
