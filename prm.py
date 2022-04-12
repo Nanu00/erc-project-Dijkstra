20adfa69c107c41807b3d448df81dd9d74ed8533
Shantanu Deshmukh <66585423+Nanu00@users.noreply.github.com>
2022-04-12 10:35:42 +0530

    Added distance limit to PRM

--- prm.py
+++ prm.py
@@ -5,10 +5,13 @@ from obstacle import Obstacle, EndArea
 class Graph(nx.Graph):
     def __init__(self, start_point: Point, obstacles: list[Obstacle], end_zone: EndArea, boundaries: tuple[Point, Point]):
         super().__init__()
+
         self.add_node(start_point)
         self.obstacles = obstacles
         self.end_zone = end_zone
         self.boundaries = boundaries
+
+        self.dist_limit = abs(min([self.boundaries[0].x - self.boundaries[1].x, self.boundaries[0].y - self.boundaries[1].y]))/5
 
     def sample(self, num: int):
         for _ in range(num):
@@ -22,8 +25,12 @@ class Graph(nx.Graph):
 
             for node in self.nodes:
                 intersections = [o.checkint(node, new_point) for o in self.obstacles]
+
                 if any(intersections):
                     continue
+                if new_point.distance(node) > self.dist_limit:
+                    continue
+
                 self.add_edge(node, new_point)
 
     def get_plottable(self):
